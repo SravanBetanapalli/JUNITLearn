00c0cdd24763878cb987e58ecbcbc6e1db46ea83
SravanBetanapalli <betanapallisravanraj@gmail.com>
2019-12-15 17:45:15 +0530

    committing two Projects files at a time.

---
+++ GitTest2/src/GitTest2.java
@@ -0,0 +1,11 @@
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I am in gitTest2 class 1st change");
+		
+	}
+
+}
---
+++ GitTest3/src/com/vogella/egit/multi/java2/GitTest3.java
@@ -0,0 +1,11 @@
+package com.vogella.egit.multi.java2;
+
+public class GitTest3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I am in gitTest3 class 1st change");
+	}
+
+}
