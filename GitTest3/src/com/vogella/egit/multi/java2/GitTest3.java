e3df2aa9f7bec8e7c1dad2f31e148578038315e6
SravanBetanapalli <betanapallisravanraj@gmail.com>
2019-12-15 18:12:14 +0530

    testing added branchTest commit

--- GitTest3/src/com/vogella/egit/multi/java2/GitTest3.java
+++ GitTest3/src/com/vogella/egit/multi/java2/GitTest3.java
@@ -5,7 +5,13 @@ public class GitTest3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		//test commit
 		System.out.println("I am in gitTest3 class 1st change");
+		
+		//added a branch-L2
+		//ksdfasfgdasgq
+		//dsakjhakdhak
 	}
 
 }
